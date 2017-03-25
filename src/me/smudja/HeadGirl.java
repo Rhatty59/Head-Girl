615fb823fea7db3777e2a6bf2200b5231838d626
Smudja <smudger06@gmail.com>
2017-03-25 21:47:02 +0000

    create main class

---
+++ src/me/smudja/HeadGirl.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package me.smudja;
+
+/**
+ * @author smithl
+ *
+ */
+public class HeadGirl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
