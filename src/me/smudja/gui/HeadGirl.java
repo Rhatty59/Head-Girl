e0c2e0ca0eea7aa6a06e0678dd43f2a040dd886f
Smudja <smudger06@gmail.com>
2017-04-09 21:50:50 +0100

    scale text to fit window

--- src/me/smudja/gui/HeadGirl.java
+++ src/me/smudja/gui/HeadGirl.java
@@ -12,10 +12,20 @@ import java.util.TimerTask;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Box;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class HeadGirl extends Application {
@@ -31,6 +41,12 @@ public class HeadGirl extends Application {
 	private static int UPDATE_FREQUENCY;
 
 	private static int REQUEST_LIMIT;
+	
+	private static int WINDOW_WIDTH;
+	
+	private static int WINDOW_HEIGHT;
+	
+	private static int FONT_SIZE;
 	
 	private Updater updater;
 
@@ -54,6 +70,9 @@ public class HeadGirl extends Application {
 				prop.setProperty("message_life", "30000");
 				prop.setProperty("update_frequency", "10000");
 				prop.setProperty("request_limit", "10");
+				prop.setProperty("window_width", "1200");
+				prop.setProperty("window_height", "600");
+				prop.setProperty("font_size", "60");
 				prop.store(output, "Configuration file for Head Girl");
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -69,6 +88,9 @@ public class HeadGirl extends Application {
 			MESSAGE_LIFE = Integer.parseInt(prop.getProperty("message_life"));
 			UPDATE_FREQUENCY = Integer.parseInt(prop.getProperty("update_frequency"));
 			REQUEST_LIMIT = Integer.parseInt(prop.getProperty("request_limit"));
+			WINDOW_WIDTH = Integer.parseInt(prop.getProperty("window_width"));
+			WINDOW_HEIGHT = Integer.parseInt(prop.getProperty("window_height"));
+			FONT_SIZE = Integer.parseInt(prop.getProperty("font_size"));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -83,15 +105,31 @@ public class HeadGirl extends Application {
 		
 		FlowPane rootNode = new FlowPane(20, 20);
 		rootNode.setAlignment(Pos.CENTER);
+		rootNode.setMaxSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		
-		primaryStage.setScene(new Scene(rootNode, 1200, 600));		
+		primaryStage.setScene(new Scene(rootNode, WINDOW_WIDTH, WINDOW_HEIGHT));
+		primaryStage.setResizable(false);
+		primaryStage.setFullScreen(true);
 		
-		TextArea display = new TextArea(updater.update());
-		display.setEditable(false);
-		display.setPrefSize(1200, 600);
-		// google "using css in javafx" to find out how to edit the text in an easy way!
+		Text text = new Text(updater.update());
 		
-		rootNode.getChildren().add(display);
+		text.setWrappingWidth(WINDOW_WIDTH);
+		text.setTextAlignment(TextAlignment.CENTER);
+		text.setFont(Font.font(FONT_SIZE));
+//		// google "using css in javafx" to find out how to edit the text in an easy way!
+//		
+        
+		rootNode.getChildren().add(text);
+        
+        Bounds textBounds = text.getBoundsInLocal();
+        
+        double font_size = text.getFont().getSize();
+        
+        while(textBounds.getHeight() > WINDOW_HEIGHT) {
+        	font_size--;
+        	text.setFont(Font.font(font_size));
+        	textBounds = text.getBoundsInLocal();
+        }
 		
 		primaryStage.show();
 		
@@ -103,14 +141,24 @@ public class HeadGirl extends Application {
 
 						@Override
 						public void run() {
-							display.setText(updater.update());
+							double font_size = FONT_SIZE;
+							
+							text.setText(updater.update());
+							text.setFont(Font.font(font_size));
+							
+							Bounds textBounds = text.getBoundsInLocal();
+							while(textBounds.getHeight() > WINDOW_HEIGHT) {
+								font_size--;
+								text.setFont(Font.font(font_size));
+								textBounds = text.getBoundsInLocal();
+							}
 						}
 		            	
 		            });
 		        }
 		    }, 0, HeadGirl.UPDATE_FREQUENCY);
 	}
-	
+
 	public static int getMaxUpdates() {
 		return MAX_UPDATES;
 	}
