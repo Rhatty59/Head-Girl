e1da9c783f130128e1dc6ccda779dfdb725379ed
Smudja <smudger06@gmail.com>
2017-06-01 13:13:12 +0100

    add APIManager to handle API requests

--- src/me/smudja/updater/PhotoUpdate.java
+++ src/me/smudja/updater/PhotoUpdate.java
@@ -3,24 +3,8 @@
  */
 package me.smudja.updater;
 
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import javax.imageio.ImageIO;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 import me.smudja.utility.LogLevel;
 import me.smudja.utility.Reporter;
@@ -55,60 +39,7 @@ public class PhotoUpdate extends Update {
 		
 		String file_id = (String) photoChoice.get("file_id");
 
-		String query;
-		try {
-			query = String.format("%s=%s",
-						URLEncoder.encode("file_id", UpdateManager.INSTANCE.charset),
-						URLEncoder.encode(file_id, UpdateManager.INSTANCE.charset)
-					);
-		} catch (UnsupportedEncodingException exc) {
-			Reporter.report("Unable to encode query. Can't check for updates this run...", LogLevel.MAJOR);
-			query = null;
-		}
-		
-		try {
-			// open connection to API and get string response
-			URLConnection connection = new URL(UpdateManager.INSTANCE.getURL() + "getFile?" + query).openConnection();
-			InputStream responseStream = connection.getInputStream();
-			BufferedReader streamReader = new BufferedReader(new InputStreamReader(responseStream, UpdateManager.INSTANCE.charset)); 
-			StringBuilder responseStrBuilder = new StringBuilder();
-
-			String inputStr;
-			while ((inputStr = streamReader.readLine()) != null) {
-				 responseStrBuilder.append(inputStr);
-			}
-			
-			JSONParser parser = new JSONParser();
-			
-			// parse response into a JSONObject
-			JSONObject response;
-			try {
-				 response = (JSONObject) parser.parse(responseStrBuilder.toString());
-			} catch (ParseException e) {
-				Reporter.report("Unable to parse reponse from API. Can't display photo...", LogLevel.MINOR);
-				response = null;
-			}
-			
-			Boolean ok = (Boolean) response.get("ok");
-			if(!ok) {
-				Reporter.report("Response from API not 'ok'. Can't display photo...", LogLevel.MINOR);
-			}
-			
-			JSONObject result = (JSONObject) response.get("result");
-			
-			String file_path = (String) result.get("file_path");
-			String token = URLEncoder.encode(UpdateManager.INSTANCE.getToken(), UpdateManager.INSTANCE.charset);
-			
-			URL url = new URL("https://api.telegram.org/file/bot" + token + "/" + file_path);
-	        
-			BufferedImage photoBuff = ImageIO.read(url);
-			
-	        photo = SwingFXUtils.toFXImage(photoBuff, null);
-		}
-		catch (IOException ioExc) {
-			Reporter.report("Unable to request photo from API (IO Exception). Can't display photo", LogLevel.MINOR);
-		}
-		
+		photo = APIManager.INSTANCE.getImageFile(file_id);
 	}
 	
 	public Image getPhoto() {
--- src/me/smudja/updater/UpdateManager.java
+++ src/me/smudja/updater/UpdateManager.java
@@ -3,14 +3,8 @@
  */
 package me.smudja.updater;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,9 +18,6 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 import me.smudja.gui.HeadGirl;
 import me.smudja.utility.LogLevel;
 import me.smudja.utility.Reporter;
@@ -40,39 +31,14 @@ public enum UpdateManager {
 	INSTANCE;
 	
 	/**
-	 * token - the telegram bot token
-	 */
-	private String token;
-	
-	/**
 	 * authorised_users - stores a long[] of valid user ids, whose messages we permit to be displayed. Stored in external file "authorised_users"
 	 */
 	private long[] authorised_users;
 	
 	/**
-	 * url - the base url for executing commands
-	 */
-	private String url;
-	
-	/**
-	 * charset - the charset to use (currently telegram uses UTF8
-	 */
-	String charset = java.nio.charset.StandardCharsets.UTF_8.name();
-	
-	/**
 	 * offset - the update to begin looking for updates from, defaults to 0 for all unconfirmed updates.
 	 */
 	long offset = 0;
-	
-	/**
-	 * limit - the maximum number of updates to get per request
-	 */
-	int limit = HeadGirl.getRequestLimit();
-	
-	/**
-	 * timeout - the time to wait for updates before giving up
-	 */
-	int timeout = HeadGirl.getTimeout();
 	
 	/**
 	 * allowed_updates - the type of update we want to receive
@@ -88,31 +54,16 @@ public enum UpdateManager {
 			if (!Files.exists(dir.resolve("config/authorised_users"))) {
 			    Files.createFile(dir.resolve("config/authorised_users"));
 			}
-			if (!Files.exists(dir.resolve("config/token"))) {
-				Files.createFile(dir.resolve("config/token"));
-				Reporter.report("No token file found. Please fill in token. Exiting...", LogLevel.INFO);
-				System.exit(1);
-			}
-			token = new String(Files.readAllBytes(dir.resolve("config/token"))).trim();
 			List<String> authUsersStr = Files.readAllLines(dir.resolve("config/authorised_users"));
 			authorised_users = new long[authUsersStr.size()];
 			for(String userStr : authUsersStr) {
 				authorised_users[authUsersStr.indexOf(userStr)] = Long.parseLong(userStr.trim());
 			}
-			url = "https://api.telegram.org/bot" + URLEncoder.encode(token, charset) + "/";
 		}
 		catch(IOException exc) {
 			Reporter.report("Error instantiating UpdateManager. Shutting down...", LogLevel.FATAL);
 			System.exit(1);
 		}
-	}
-	
-	public String getURL() {
-		return url;
-	}
-	
-	public String getToken() {
-		return token;
 	}
 	
 	/**
@@ -156,108 +107,55 @@ public enum UpdateManager {
 	 * 
 	 * @return an array containing all updates from a single request to the API
 	 */
-	private Update[] getUpdate() {
-		String query;
-		try {
-			query = String.format("%s=%s&%s=%s&%s=%s&%s=%s",
-						URLEncoder.encode("offset", charset),
-						URLEncoder.encode(Long.toString(offset), charset),
-						URLEncoder.encode("limit", charset),
-						URLEncoder.encode(Integer.toString(limit), charset),
-						URLEncoder.encode("timeout", charset),
-						URLEncoder.encode(Integer.toString(timeout), charset),
-						URLEncoder.encode("allowed_updates", charset),
-						URLEncoder.encode("[\"" + String.join("\", \"", allowed_updates) + "\"]", charset)
-					);
-		} catch (UnsupportedEncodingException exc) {
-			Reporter.report("Unable to encode query. Can't check for updates this run...", LogLevel.MAJOR);
-			return null;
-		}
-		
-		try {
-			// open connection to API and get string response
-			URLConnection connection = new URL(url + "getUpdates?" + query).openConnection();
-			InputStream responseStream = connection.getInputStream();
-			BufferedReader streamReader = new BufferedReader(new InputStreamReader(responseStream, charset)); 
-			StringBuilder responseStrBuilder = new StringBuilder();
+	private Update[] getUpdate() {	
+		JSONArray result = APIManager.INSTANCE.getUpdate(offset, HeadGirl.getRequestLimit(), HeadGirl.getTimeout(), allowed_updates);
 
-			String inputStr;
-			while ((inputStr = streamReader.readLine()) != null) {
-				 responseStrBuilder.append(inputStr);
-			}
-			
-			JSONParser parser = new JSONParser();
-			
-			// parse response into a JSONObject
-			JSONObject response;
-			try {
-				 response = (JSONObject) parser.parse(responseStrBuilder.toString());
-			} catch (ParseException e) {
-				Reporter.report("Unable to parse reponse from API. Can't check for updates this run...", LogLevel.MINOR);
-				return null;
-			}
-			
-			Boolean ok = (Boolean) response.get("ok");
-			if(!ok) {
-				Reporter.report("Response from API not 'ok'. Can't check for updates this run...", LogLevel.MINOR);
-				return null;
-			}
-			
-			JSONArray result = (JSONArray) response.get("result");
-			
-			// if we have no new updates, return
-			Boolean updated = (result.size() == 0 ? false : true);
-			if(!updated) {
-				return null;
-			}
-			
-			// if we have updates set the offset such that these updates are confirmed so won't appear again
-			if(ok && updated) {
-				offset = ((long)((JSONObject)result.get(result.size() - 1)).get("update_id")) + 1;
-			}
-			
-			ArrayList<Update> updatesList = new ArrayList<Update>();
-			
-			@SuppressWarnings("unchecked")
-			Iterator<JSONObject> iterator = result.iterator();
-			
-			// for each update, convert it into an Update instance and add to arraylist
-			
-			JSONObject update;
-			JSONObject message;
-			
-			while(iterator.hasNext()) {
-				update = iterator.next();
-				message = (JSONObject) update.get("message");
-				if (message.containsKey("text")) {
-					updatesList.add(new TextUpdate(update));
-				}
-				else if (message.containsKey("photo")) {
-					updatesList.add(new PhotoUpdate(update));
-				}
-				else {
-					Reporter.report("User sent invalid message (no text or photo field)", LogLevel.INFO);
-				}
-			}
-			
-			// remove messages from unauthorised users
-			Iterator<Update> updateIterator = updatesList.iterator();
-			while(updateIterator.hasNext()) {
-				Update sel = updateIterator.next();
-				if(!(LongStream.of(authorised_users).anyMatch(x -> x == sel.getUserID()))) {
-					Reporter.report("Unauthorised user sent message to bot! User ID: " + sel.getUserID(), LogLevel.INFO);
-					updateIterator.remove();
-				}
-			}
-			
-			Update[] updates = new Update[]{};
-			
-			return updatesList.toArray(updates);
-		} catch (IOException ioExc) {
-			Reporter.report("Unable to request update from API (IO Exception). Can't check for updates this run...", LogLevel.MINOR);
+		// if we have no new updates, return
+		Boolean updated = (result.size() == 0 ? false : true);
+		if(!updated) {
 			return null;
 		}
-	}
+
+		offset = ((long)((JSONObject)result.get(result.size() - 1)).get("update_id")) + 1;
+
+		ArrayList<Update> updatesList = new ArrayList<Update>();
+
+		@SuppressWarnings("unchecked")
+		Iterator<JSONObject> iterator = result.iterator();
+
+		// for each update, convert it into an Update instance and add to arraylist
+
+		JSONObject update;
+		JSONObject message;
+
+		while(iterator.hasNext()) {
+			update = iterator.next();
+			message = (JSONObject) update.get("message");
+			if (message.containsKey("text")) {
+				updatesList.add(new TextUpdate(update));
+			}
+			else if (message.containsKey("photo")) {
+				updatesList.add(new PhotoUpdate(update));
+			}
+			else {
+				Reporter.report("User sent invalid message (no text or photo field)", LogLevel.INFO);
+			}
+		}
+
+		// remove messages from unauthorised users
+		Iterator<Update> updateIterator = updatesList.iterator();
+		while(updateIterator.hasNext()) {
+			Update sel = updateIterator.next();
+			if(!(LongStream.of(authorised_users).anyMatch(x -> x == sel.getUserID()))) {
+				Reporter.report("Unauthorised user sent message to bot! User ID: " + sel.getUserID(), LogLevel.INFO);
+				updateIterator.remove();
+			}
+		}
+
+		Update[] updates = new Update[]{};
+
+		return updatesList.toArray(updates);
+}
 	
 	/**
 	 * Plays the ringtone
